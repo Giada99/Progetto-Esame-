03771f882193bde740fe3fc3067e25a2fbb3619c
S1090231 <62309778+S1090231@users.noreply.github.com>
2020-05-25 16:06:40 +0100

    Upload Initial Project
    Caricamento struttura iniziale del progetto tramite SpringInitializr.

---
+++ exam/src/main/java/it/project/exam/controller/SimpleRestController.java
@@ -0,0 +1,10 @@
+package it.project.exam.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleRestController {
+	@GetMapping
+
+}
